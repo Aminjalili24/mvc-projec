f4995bec8dedf9abfc9191e19f48605142d8facd
aminjalili24 <amin.jalili137819@gmail.com>
2023-07-07 17:33:33 +0330

    HasMethodCaller function setsetAllowedMethod added

--- system/Database/Traits/HasMethodCaller.php
+++ system/Database/Traits/HasMethodCaller.php
@@ -3,5 +3,12 @@
 namespace System\Database\Traits;
 
 trait HasMethodCaller{
+    private $allMethods = ['create', 'update', 'delete', 'find', 'all', 'save', 'where', 'whereOr', 'whereIn', 'whereNull', 'whereNotNull', 'limit', 'orderBy', 'get', 'paginate'];
+    private $allowedMethods = ['create', 'update', 'delete', 'find', 'all', 'save', 'where', 'whereOr', 'whereIn', 'whereNull', 'whereNotNull', 'limit', 'orderBy', 'get', 'paginate'];
 
+
+
+    protected function setAllowedMethod($array){
+        $this->allowedMethods = $array;
+    }
 }
